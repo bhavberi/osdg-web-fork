bd56033ca3317908573ccd2bc5cc041bd94c7f82
PraneethJain <praneethjain005@gmail.com>
2024-02-07 23:18:23 +0530

    feat: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import brandLogo from "@/assets/BrandLogo.png";
+
 export default function Navbar() {
-  return <h1>Navbar</h1>;
+  const linkClass = "opacity-85 hover:opacity-100 text-xl";
+
+  return (
+    <nav className="flex justify-between items-center p-4 bg-[#2E303E] text-white">
+      <Image src={brandLogo} alt="OSDG" className="w-20" />
+      <div className="w-1/3 flex justify-evenly">
+        <Link className={linkClass} href="/">
+          Home
+        </Link>
+        <Link className={linkClass} href="/projects">
+          Projects
+        </Link>
+        <Link className={linkClass} href="/events">
+          Events
+        </Link>
+        <Link className={linkClass} href="/team">
+          Team
+        </Link>
+      </div>
+    </nav>
+  );
 }
