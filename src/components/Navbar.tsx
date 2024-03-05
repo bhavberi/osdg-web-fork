6fb295c4f4d404e4bb61fbf93147a64132dd79ca
PraneethJain <praneethjain005@gmail.com>
2024-03-05 12:02:36 +0530

    feat: redo mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,9 @@ export default function Navbar() {
   const linkClass = "opacity-85 hover:opacity-100 text-xl";
 
   return (
-    <nav className="flex justify-between items-center p-4 bg-[#2E303E] text-white">
-      <Image src={brandLogo} alt="OSDG" className="w-20" />
-      <div className="w-2/3 md:w-1/3 flex justify-evenly">
+    <nav className="flex flex-col md:flex-row justify-between items-center p-4 bg-[#2E303E] text-white">
+      <Image src={brandLogo} alt="OSDG" className="w-24 pb-4 md:pb-0" />
+      <div className="w-full md:w-1/3 flex justify-evenly">
         <Link className={linkClass} href="/">
           Home
         </Link>
