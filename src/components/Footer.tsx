f360c9f005ec3b5ab17868d4b0fa47e2574df7e0
PraneethJain <praneethjain005@gmail.com>
2024-03-05 11:50:03 +0530

    feat: footer redesign

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,28 +6,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-around px-32 py-12 bg-slate-700 text-white">
+    <footer className="flex flex-col md:flex-row items-center justify-between md:px-32 py-12 bg-slate-700 text-white">
       <div className="text-xs h-auto">
-        <Image src={brandLogo} alt="OSDG" className="w-32 mb-6" />
-        <p>&copy; 2024</p>
-        <p>Open Source Developers Group</p>
-        <p>International Institute of Information</p>
-        <p>Technology, Hyderabad</p>
+        <Image src={brandLogo} alt="OSDG" className="w-24 mb-6" />
       </div>
-      <div className="flex h-auto justify-center items-center">
+      <div className="flex h-auto justify-center items-center pb-4">
         {socials.map(([link, icon], idx) => (
           <a
-            href={link}
-            key={idx}
-            className="mx-2 opacity-60 hover:opacity-100 w-8 h-8"
+          href={link}
+          key={idx}
+          className="mx-2 opacity-60 hover:opacity-100 w-8 h-8"
           >
             <FontAwesomeIcon icon={icon} />
           </a>
         ))}
       </div>
-      <div className="h-auto flex flex-col justify-center items-end">
-        <p className="text-l font-bold">Reach Us</p>
-        <a  className="text-xs" href="mailto:osdg@students.iiit.ac.in">osdg@students.iiit.ac.in</a>
+      <div className="h-auto flex flex-col justify-center text-center">
+        <p>&copy; 2024</p>
+        <p>Open Source Developers Group</p>
+        <p>International Institute of Information</p>
+        <p>Technology, Hyderabad</p>
       </div>
     </footer>
   );
--- src/utils/socials.ts
+++ src/utils/socials.ts
@@ -5,12 +5,14 @@ import {
   faLinkedin,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const socials: [string, IconDefinition][] = [
   ["https://www.facebook.com/groups/185567594878116", faFacebook],
   ["https://github.com/OSDG-IIITH", faGithub],
   ["https://www.linkedin.com/company/74330374", faLinkedin],
   ["https://www.instagram.com/osdg.iiith", faInstagram],
+  ["mailto:osdg@students.iiit.ac.in", faEnvelope],
 ];
 
 export { socials };
