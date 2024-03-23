a7236e972446e5d481fe12aefa845513fa1c73a5
Aryan Maskara <aryan.maskara@gmail.com>
2024-03-23 23:48:44 +0530

    made project panel dynamic

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -20,10 +20,10 @@ export default function Projects() {
                 Project Title
               </h2>
             </div>
-          </div>
+          </div> 
 
           {/* Text content beside image */}
-          <div className="lg:flex lg:ml-4 flex flex-col justify-between lg:px-10">
+          <div className="hidden lg:flex lg:ml-4 flex flex-col justify-between lg:px-10">
             <h2 className="lg:text-5xl font-semibold break-all text-wrap">
               Project Title
             </h2>
@@ -160,264 +160,41 @@ export default function Projects() {
       <div>
         {/* Projects */}
         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 place-items-center mb-5">
-          <div className="h-[350px] w-[350px] bg-[#2a284b] rounded-xl relative m-5 flex flex-col justify-between">
-            <div className="flex justify-evenly items-center p-2 w-full">
-              <div className="h-24 w-24 bg-black rounded-xl shrink-0 m-2.5"></div>
-              <div className="flex flex-col justify-center ml-5 shrink">
-                <h2 className="text-3xl font-semibold text-white mt-3 mb-2">
-                  Project Title
-                </h2>
-                <div className="flex flex-wrap mb-3">
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    React
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Javascript
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Python
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Cryptography
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#4b2599] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    +3
+
+          {projects.map((project, index) => (
+            <div key = {index} className="h-[350px] w-[350px] bg-[#2a284b] rounded-xl relative m-5 flex flex-col justify-between">
+              <div className="flex justify-evenly items-center p-2 w-full">
+                {/* <div className="h-24 w-24 bg-black rounded-xl shrink-0 m-2.5"></div> */}
+                <img src="https://clubs.iiit.ac.in/_next/image?url=http%3A%2F%2Ffiles%2Ffiles%2Fdownload%3Ffilename%3D572YeG2MbymmpyV5b8MQJh_osdg.png&w=384&q=75" className="object-cover h-24 w-24 bg-black rounded-xl shrink-0 m-2.5" />
+                <div className="flex flex-col justify-center ml-5 shrink">
+                  <h2 className="text-3xl font-semibold text-white mt-3 mb-2">
+                    {project.projectName}
+                  </h2>
+                  <div className="flex flex-wrap mb-3">
+                    {project.languages && project.languages.map((language, index_languages) => (
+                      <div key = {index_languages} className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
+                        {language}
+                      </div>
+                    ))}
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="flex flex-col justify-center ml-5 mb-2 items-start w-full text-[#cbb3fa] italic underline text-s">
-              <a href="https://github.com/bunny8469/seddit-app">
-                https://github.com/bunny8469/seddit-app
-              </a>
-            </div>
-            <div className="flex flex-col justify-start items-start ml-5 text-white text-xs">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Some
-              Brief Description
-            </div>
-            <div className="flex gap-2 mx-5 my-5 justify-start items-center w-2/3 leading-5">
-              <div className="h-14 w-14 bg-red-300 rounded-full shrink-0"></div>
-              <div className="text-l font-semibold text-white">
-                Open Source Developers Group
+              <div className="flex flex-col justify-center ml-5 mb-2 items-start w-full text-[#cbb3fa] italic underline text-s">
+                <a href={project.link}>
+                  {project.link}
+                </a>
               </div>
-            </div>
-          </div>
-          <div className="h-[350px] w-[350px] bg-[#2a284b] rounded-xl relative m-5 flex flex-col justify-between">
-            <div className="flex justify-evenly items-center p-2 w-full">
-              <div className="h-24 w-24 bg-black rounded-xl shrink-0 m-2.5"></div>
-              <div className="flex flex-col justify-center ml-5 shrink">
-                <h2 className="text-3xl font-semibold text-white mt-3 mb-2">
-                  Project Title
-                </h2>
-                <div className="flex flex-wrap mb-3">
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    React
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Javascript
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Python
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Cryptography
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#4b2599] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    +3
-                  </div>
+              <div className="flex flex-col justify-start items-start ml-5 text-white text-xs">
+                {project.description}
+              </div>
+              <div className="flex gap-2 mx-5 my-5 justify-start items-center w-2/3 leading-5">
+                <img src="https://clubs.iiit.ac.in/_next/image?url=http%3A%2F%2Ffiles%2Ffiles%2Fdownload%3Ffilename%3D572YeG2MbymmpyV5b8MQJh_osdg.png&w=384&q=75" className="object-cover h-14 w-14 bg-red-300 rounded-full shrink-0" />
+                <div className="text-l font-semibold text-white">
+                  Open Source Developers Group
                 </div>
               </div>
             </div>
-            <div className="flex flex-col justify-center ml-5 mb-2 items-start w-full text-[#cbb3fa] italic underline text-s">
-              <a href="https://github.com/bunny8469/seddit-app">
-                https://github.com/bunny8469/seddit-app
-              </a>
-            </div>
-            <div className="flex flex-col justify-start items-start ml-5 text-white text-xs">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Some
-              Brief Description
-            </div>
-            <div className="flex gap-2 mx-5 my-5 justify-start items-center w-2/3 leading-5">
-              <div className="h-14 w-14 bg-yellow-300 rounded-full shrink-0"></div>
-              <div className="text-l font-semibold text-white">
-                Open Source Developers Group
-              </div>
-            </div>
-          </div>
-          <div className="h-[350px] w-[350px] bg-[#2a284b] rounded-xl relative m-5 flex flex-col justify-between">
-            <div className="flex justify-evenly items-center p-2 w-full">
-              <div className="h-24 w-24 bg-black rounded-xl shrink-0 m-2.5"></div>
-              <div className="flex flex-col justify-center ml-5 shrink">
-                <h2 className="text-3xl font-semibold text-white mt-3 mb-2">
-                  Project Title
-                </h2>
-                <div className="flex flex-wrap mb-3">
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    React
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Javascript
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Python
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Cryptography
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#4b2599] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    +3
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col justify-center ml-5 mb-2 items-start w-full text-[#cbb3fa] italic underline text-s">
-              <a href="https://github.com/bunny8469/seddit-app">
-                https://github.com/bunny8469/seddit-app
-              </a>
-            </div>
-            <div className="flex flex-col justify-start items-start ml-5 text-white text-xs">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Some
-              Brief Description
-            </div>
-            <div className="flex gap-2 mx-5 my-5 justify-start items-center w-2/3 leading-5">
-              <div className="h-14 w-14 bg-green-300  rounded-full shrink-0"></div>
-              <div className="text-l font-semibold text-white">
-                Open Source Developers Group
-              </div>
-            </div>
-          </div>
-          <div className="h-[350px] w-[350px] bg-[#2a284b] rounded-xl relative m-5 flex flex-col justify-between">
-            <div className="flex justify-evenly items-center p-2 w-full">
-              <div className="h-24 w-24 bg-black rounded-xl shrink-0 m-2.5"></div>
-              <div className="flex flex-col justify-center ml-5 shrink">
-                <h2 className="text-3xl font-semibold text-white mt-3 mb-2">
-                  Project Title
-                </h2>
-                <div className="flex flex-wrap mb-3">
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    React
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Javascript
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Python
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Cryptography
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#4b2599] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    +3
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col justify-center ml-5 mb-2 items-start w-full text-[#cbb3fa] italic underline text-s">
-              <a href="https://github.com/bunny8469/seddit-app">
-                https://github.com/bunny8469/seddit-app
-              </a>
-            </div>
-            <div className="flex flex-col justify-start items-start ml-5 text-white text-xs">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Some
-              Brief Description
-            </div>
-            <div className="flex gap-2 mx-5 my-5 justify-start items-center w-2/3 leading-5">
-              <div className="h-14 w-14 bg-blue-300  rounded-full shrink-0"></div>
-              <div className="text-l font-semibold text-white">
-                Nijesh Raghava
-              </div>
-            </div>
-          </div>
-          <div className="h-[350px] w-[350px] bg-[#2a284b] rounded-xl relative m-5 flex flex-col justify-between">
-            <div className="flex justify-evenly items-center p-2 w-full">
-              <div className="h-24 w-24 bg-black rounded-xl shrink-0 m-2.5"></div>
-              <div className="flex flex-col justify-center ml-5 shrink">
-                <h2 className="text-3xl font-semibold text-white mt-3 mb-2">
-                  Project Title
-                </h2>
-                <div className="flex flex-wrap mb-3">
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    React
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Javascript
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Python
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Cryptography
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#4b2599] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    +3
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col justify-center ml-5 mb-2 items-start w-full text-[#cbb3fa] italic underline text-s">
-              <a href="https://github.com/bunny8469/seddit-app">
-                https://github.com/bunny8469/seddit-app
-              </a>
-            </div>
-            <div className="flex flex-col justify-start items-start ml-5 text-white text-xs">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Some
-              Brief Description
-            </div>
-            <div className="flex gap-2 mx-5 my-5 justify-start items-center w-2/3 leading-5">
-              <div className="h-14 w-14 bg-red-300  rounded-full shrink-0"></div>
-              <div className="text-l font-semibold text-white">
-                Vamseedhar Vanarasi
-              </div>
-            </div>
-          </div>
-          <div className="h-[350px] w-[350px] bg-[#2a284b] rounded-xl relative m-5 flex flex-col justify-between">
-            <div className="flex justify-evenly items-center p-2 w-full">
-              <div className="h-24 w-24 bg-black rounded-xl shrink-0 m-2.5"></div>
-              <div className="flex flex-col justify-center ml-5 shrink">
-                <h2 className="text-3xl font-semibold text-white mt-3 mb-2">
-                  Project Title
-                </h2>
-                <div className="flex flex-wrap mb-3">
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    React
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Javascript
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Python
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#5e18eb] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    Cryptography
-                  </div>
-                  <div className="flex justify-center items-center p-1 m-1 h-6 w-min bg-[#4b2599] rounded-md relative my-1 text-white text-[0.65rem] font-bold">
-                    +3
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col justify-center ml-5 mb-2 items-start w-full text-[#cbb3fa] italic underline text-s">
-              <a href="https://github.com/bunny8469/seddit-app">
-                https://github.com/bunny8469/seddit-app
-              </a>
-            </div>
-            <div className="flex flex-col justify-start items-start ml-5 text-white text-xs">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Some
-              Brief Description
-            </div>
-            <div className="flex gap-2 mx-5 my-5 justify-start items-center w-2/3 leading-5">
-              <div className="h-14 w-14 bg-yellow-300  rounded-full shrink-0"></div>
-              <div className="text-l font-semibold text-white">
-                Open Source Developers Group
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </main>
