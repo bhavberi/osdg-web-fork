d0fcb03f2b356cd6214511234727503c68f31589
Mohit Singh <unignoramus@gmail.com>
2024-03-24 22:37:17 +0530

    Finish up the projects carousel and other minor tweaks

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -25,32 +25,39 @@ export default function Projects({ searchParams }) {
 
   return (
     <main className="bg-[#11101f] min-h-[100vh] flex flex-col justify-center items-center">
-      <div className="bg-[#211248] text-white lg:h-[60vh] px-5 lg:p-20 m-10 lg:m-20 rounded-3xl w-screen flex flex-col justify-center max-w-[80vw]">
-        <div className="flex flex-col lg:flex-row items-center justify-between lg:items-start">
-          <div className="hidden lg:flex lg:items-center h-20 w-20 lg:h-48 lg:w-48 bg-gradient-to-br from-[#02b5ff] to-[#5c1aeb] rounded-3xl relative shrink-0"></div>
-          <div className="hidden lg:flex lg:ml-4 lg:flex-col justify-between lg:px-10">
-            <h2 className="lg:text-5xl font-semibold break-all text-wrap">
+      <div className="bg-[#211248] text-white lg:h-[60vh] px-5 lg:p-10 m-10 lg:m-20 rounded-3xl w-screen flex flex-col justify-center max-w-[80vw]">
+        <div className="flex flex-col lg:flex-row items-center justify-start h-full align-middle">
+          <div
+            className="hidden mx-5 lg:flex lg:items-center h-[90%] w-[30%] bg-gradient-to-br from-[#02b5ff] to-[#5c1aeb] rounded-3xl relative shrink-0"
+            style={{
+              backgroundImage: `url(${
+                carouselProjectData[carouselIndex].image
+                  ? carouselProjectData[carouselIndex].image
+                  : "https://clubs.iiit.ac.in/_next/image?url=http%3A%2F%2Ffiles%2Ffiles%2Fdownload%3Ffilename%3D572YeG2MbymmpyV5b8MQJh_osdg.png&w=384&q=75"
+              })`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+          <div className="hidden lg:flex lg:ml-10 lg:flex-col justify-around lg:px-10 h-full">
+            <h2 className="lg:text-5xl font-semibold text-wrap">
               {carouselProjectData[carouselIndex].projectName}
             </h2>
-            <div className="lg:text-sm max-w-[50vw] lg:mx--10 lg:mt-5">
+            <div className="max-w-[50vw] lg:mx--10 lg:mt-5 text-justify">
               {carouselProjectData[carouselIndex].description}
             </div>
-            <div className="flex justify-center lg:justify-start flex-wrap">
-              <span className="lg:text-lg italic mr-1 text-nowrap">
-                Maintained by
-              </span>
-              <span className="lg:text-lg italic font-semibold">
-                {carouselProjectData[carouselIndex].maintainers
-                  .slice(0, -1)
-                  .map((maintainer) => maintainer.name)
-                  .join(", ")}
-                {carouselProjectData[carouselIndex].maintainers.length > 1
-                  ? " and " +
-                    carouselProjectData[carouselIndex].maintainers[
-                      carouselProjectData[carouselIndex].maintainers.length - 1
-                    ].name
-                  : carouselProjectData[carouselIndex].maintainers[0].name}
-              </span>
+            <div className="flex justify-center lg:justify-start flex-wrap lg:text-lg italic">
+              Maintained by&nbsp;
+              {carouselProjectData[carouselIndex].maintainers
+                .slice(0, -1)
+                .map((maintainer) => maintainer.name)
+                .join(", ")}
+              {carouselProjectData[carouselIndex].maintainers.length > 1
+                ? " and " +
+                  carouselProjectData[carouselIndex].maintainers[
+                    carouselProjectData[carouselIndex].maintainers.length - 1
+                  ].name
+                : carouselProjectData[carouselIndex].maintainers[0].name}
             </div>
             <div className="flex items-center">
               <a href={carouselProjectData[carouselIndex].link}>
@@ -58,8 +65,7 @@ export default function Projects({ searchParams }) {
                   View Project
                 </div>
               </a>
-              <div className="flex gap-2 mx-10">
-                {/* get all the pfps */}
+              <div className="flex gap-2 mx-10 flex-wrap select-none">
                 {carouselProjectData[carouselIndex].maintainers.map(
                   (maintainer, index) => (
                     <img
@@ -77,63 +83,92 @@ export default function Projects({ searchParams }) {
             </div>
           </div>
 
-          <div className="flex lg:hidden items-start justify-start h-[25vh] w-[25vh] lg:h-48 lg:w-48 bg-gradient-to-br from-[#02b5ff] to-[#5c1aeb] rounded-3xl relative mt-10"></div>
-          <div className="flex lg:hidden mt-4 items-center justify-center flex-col">
-            <h2 className="text-2xl font-semibold break-all text-wrap text-center">
+          <div
+            className="flex lg:hidden items-start justify-start h-[25vh] w-[25vh] lg:h-48 lg:w-48 bg-gradient-to-br from-[#02b5ff] to-[#5c1aeb] rounded-3xl relative mt-10"
+            style={{
+              backgroundImage: `url(${
+                carouselProjectData[carouselIndex].image
+                  ? carouselProjectData[carouselIndex].image
+                  : "https://clubs.iiit.ac.in/_next/image?url=http%3A%2F%2Ffiles%2Ffiles%2Fdownload%3Ffilename%3D572YeG2MbymmpyV5b8MQJh_osdg.png&w=384&q=75"
+              })`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+          <div className="flex lg:hidden mt-6 items-center justify-center flex-col">
+            <h2 className="text-4xl font-semibold text-wrap text-center">
               {carouselProjectData[carouselIndex].projectName}
             </h2>
-            <div className="text-sm">
+            <div className="text-sm m-5 text-justify">
               {carouselProjectData[carouselIndex].description}
             </div>
-            <div className="flex justify-center flex-wrap">
-              <span className="text-lg italic mr-1 text-nowrap">
-                Maintained by
-              </span>
-              <span className="text-lg italic font-semibold">
-                {carouselProjectData[carouselIndex].maintainers
-                  .slice(0, -1)
-                  .map((maintainer) => maintainer.name)
-                  .join(", ")}
-                {carouselProjectData[carouselIndex].maintainers.length > 1
-                  ? " and " +
-                    carouselProjectData[carouselIndex].maintainers[
-                      carouselProjectData[carouselIndex].maintainers.length - 1
-                    ].name
-                  : carouselProjectData[carouselIndex].maintainers[0].name}
-              </span>
-            </div>
-            <div className="flex items-center">
+            <div className="flex items-center mx-5 mb-5">
               <a href={carouselProjectData[carouselIndex].link}>
                 <div className="bg-[#5e18eb] max-w-[40vw] font-semibold rounded-3xl p-5 flex justify-center items-center select-none cursor-pointer">
                   View Project
                 </div>
               </a>
             </div>
-            <div className="flex gap-2 mx-10 my-10">
-              <div className="h-10 w-10 bg-red-300 rounded-full"></div>
-              <div className="h-10 w-10 bg-yellow-300 rounded-full"></div>
-              <div className="h-10 w-10 bg-green-300 rounded-full"></div>
-              <div className="h-10 w-10 bg-blue-300 rounded-full"></div>
+            <div className="flex justify-center flex-wrap italic px-5 text-center">
+              Maintained by&nbsp;
+              {carouselProjectData[carouselIndex].maintainers
+                .slice(0, -1)
+                .map((maintainer) => maintainer.name)
+                .join(", ")}
+              {carouselProjectData[carouselIndex].maintainers.length > 1
+                ? " and " +
+                  carouselProjectData[carouselIndex].maintainers[
+                    carouselProjectData[carouselIndex].maintainers.length - 1
+                  ].name
+                : carouselProjectData[carouselIndex].maintainers[0].name}
+            </div>
+            <div className="flex gap-2 mx-10 mb-10 mt-5 flex-wrap justify-center">
+              {carouselProjectData[carouselIndex].maintainers.map(
+                (maintainer, index) => (
+                  <img
+                    key={index}
+                    src={
+                      maintainer.pfpURL
+                        ? maintainer.pfpURL
+                        : `https://robohash.org/${maintainer.name}.png?set=set5`
+                    }
+                    className="object-cover h-16 w-16 bg-red-300 rounded-full m-3"
+                  />
+                )
+              )}
             </div>
           </div>
         </div>
       </div>
-      <div className="flex gap-5 place-items-center flex-wrap w-full align-middle justify-center mb-10 select-none">
+      <div className="flex gap-5 place-items-center flex-wrap w-[80vw] align-middle justify-center mb-10 select-none">
         {carouselProjectData.map((project, index) => {
           return (
             <div
               key={index}
-              className={`h-36 w-56 bg-gradient-to-br from-[#909fdf] to-[#5c1aeb] rounded-3xl relative m-5 cursor-pointer ${
+              className={`h-24 w-28 lg:h-36 lg:w-56 relative m-5 cursor-pointer ${
                 index === carouselIndex ? "opacity-100" : "opacity-50"
               }`}
               onClick={() => setCarouselIndex(index)}
             >
               <div
+                className={`absolute inset-0 rounded-3xl`}
+                style={{
+                  backgroundImage: `url(${
+                    project.image
+                      ? project.image
+                      : "https://clubs.iiit.ac.in/_next/image?url=http%3A%2F%2Ffiles%2Ffiles%2Fdownload%3Ffilename%3D572YeG2MbymmpyV5b8MQJh_osdg.png&w=384&q=75"
+                  })`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  filter: index === carouselIndex ? "blur(5px)" : "none",
+                }}
+              ></div>
+              <div
                 className={`flex flex-col justify-center items-center p-5 absolute top-0 left-0 h-full w-full fd-sh hover:opacity-100 transition-all bg-opacity-0 rounded-3xl ${
                   index === carouselIndex ? "opacity-100" : "opacity-0"
                 }`}
               >
-                <h2 className="text-xl font-semibold break-all text-white">
+                <h2 className="text-xl font-semibold text-white">
                   {project.projectName}
                 </h2>
               </div>
@@ -141,7 +176,7 @@ export default function Projects({ searchParams }) {
           );
         })}
       </div>
-      <div className="flex flex-col w-full pl-16">
+      <div className="flex flex-col w-full pl-8 lg:pl-16">
         <h2 className="my-5 mx-0 text-3xl lg:text-5xl font-semibold break-all text-white">
           Projects Panel
         </h2>
@@ -184,7 +219,11 @@ export default function Projects({ searchParams }) {
                   >
                     <div className="flex items-center p-2 w-full">
                       <img
-                        src="https://clubs.iiit.ac.in/_next/image?url=http%3A%2F%2Ffiles%2Ffiles%2Fdownload%3Ffilename%3D572YeG2MbymmpyV5b8MQJh_osdg.png&w=384&q=75"
+                        src={
+                          project.image
+                            ? project.image
+                            : "https://clubs.iiit.ac.in/_next/image?url=http%3A%2F%2Ffiles%2Ffiles%2Fdownload%3Ffilename%3D572YeG2MbymmpyV5b8MQJh_osdg.png&w=384&q=75"
+                        }
                         className="object-cover h-24 w-24 bg-black rounded-xl shrink-0 m-2.5"
                       />
                       <div className="flex flex-col justify-center ml-5 shrink">
@@ -220,7 +259,7 @@ export default function Projects({ searchParams }) {
                         {project.link}
                       </a>
                     </div>
-                    <div className="mx-5 text-white text-s">
+                    <div className="mx-5 text-white text-s text-justify">
                       {project.description}
                     </div>
                     {project.category === "OSDG" && (
