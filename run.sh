5cb2ef81a956a2ce4af527cd1e3e6bb5952cd651
Ankith <itsankith26@gmail.com>
2024-03-25 00:49:16 +0530

    Replace cas with web in docker-compose

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ if [[ $WEBSITE_DIR == "" ]]; then
     WEBSITE_DIR=.
 fi
 
-docker-compose -f $WEBSITE_DIR/docker-compose.yml -p cas up --build
+docker-compose -f $WEBSITE_DIR/docker-compose.yml -p web up --build
